44b3ad38e7cfe0eb4cf0dfb5142f82dcd4ab624e
Peter Lemiszki <plemiszki@gmail.com>
2019-03-24 13:36:49 -0400

    add message component

---
+++ src/containers/message.jsx
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import Common from './modules/common.js';
+
+class Message extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    let initialState = {
+      message: '',
+      color: ''
+    }
+
+    this.state = initialState;
+  }
+
+  componentDidMount() {
+    let message = localStorage.getItem('message');
+    if (message) {
+      this.setState({
+        message,
+        color: localStorage.getItem('message-color')
+      }, () => {
+        localStorage.removeItem('message');
+        localStorage.removeItem('message-color');
+      });
+    }
+  }
+
+  render() {
+    if (this.state.message) {
+      let lines = this.state.message.split("\n");
+      return(
+        <div className={ `message-box${ this.state.color ? ` ${this.state.color}` : ''}` }>
+          <div className="message-close" onClick={ Common.changeState.bind(this, 'message', '') }></div>
+          { lines.map((line, index) => {
+            return(
+              <p key={ index }>{ line }</p>
+            );
+          }) }
+        </div>
+      );
+    } else {
+      return null;
+    }
+  }
+}
+
+export default Message;
--- src/index.js
+++ src/index.js
@@ -5,10 +5,12 @@ import Index from './containers/modules/index.js';
 import Details from './containers/modules/details.jsx';
 
 import StandardIndex from './containers/standard-index.jsx';
+import Message from './containers/message.jsx';
 
 export {
   Common,
   Details,
   Index,
+  Message,
   StandardIndex
 }
