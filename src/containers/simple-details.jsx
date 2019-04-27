56f36965f2ecfc20fcd1f0971716dec7580e7ff9
Peter Lemiszki <plemiszki@gmail.com>
2019-04-27 09:03:27 -0400

    fix children bug

--- src/containers/simple-details.jsx
+++ src/containers/simple-details.jsx
@@ -129,7 +129,7 @@ class SimpleDetails extends React.Component {
             { this.renderCopyButton.call(this) }
           </div>
         </div>
-        { this.renderCopyModal.call(this) }
+        { this.renderCopyModal.call(this, children) }
       </div>
     );
   }
@@ -152,7 +152,7 @@ class SimpleDetails extends React.Component {
     }
   }
 
-  renderCopyModal() {
+  renderCopyModal(children) {
     if (this.props.children) {
       return(
         <Modal isOpen={ this.state.copyModalOpen } onRequestClose={ Common.closeModals.bind(this) } contentLabel="Modal" style={ Common.newEntityModalStyles(this.props.modalDimensions, this.props.modalRows) }>
