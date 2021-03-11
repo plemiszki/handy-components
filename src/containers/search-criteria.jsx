7cb19abc1b90c74d9b841b3b52bde536dc61b7e8
Peter Lemiszki <plemiszki@gmail.com>
2021-03-11 17:32:52 -0500

    number range field type

--- src/containers/search-criteria.jsx
+++ src/containers/search-criteria.jsx
@@ -27,13 +27,27 @@ class SearchCriteria extends React.Component {
     }
   }
 
+  stringIsNumber(string) {
+    return !isNaN(string) && !isNaN(parseFloat(string));
+  }
+
   validateCriteria(criteria) {
-    const keys = Object.keys(criteria);
-    keys.forEach((key) => {
-      if (criteria[key].value.trim() === '') {
-        delete criteria[key];
+    for (const [key, obj] of Object.entries(criteria)) {
+      if (obj.minValue) { // number range
+        let { minValue, maxValue } = obj;
+        if (this.stringIsNumber(minValue) == false || this.stringIsNumber(maxValue) == false) {
+          delete criteria[key];
+        } else if (+maxValue < +minValue) {
+          delete criteria[key];
+        }
+      } else if (obj.value) { // string
+        if (criteria[key].value.trim() === '') {
+          delete criteria[key];
+        } else {
+          criteria[key].value = criteria[key].value.trim();
+        }
       }
-    });
+    }
     return criteria;
   }
 
@@ -53,14 +67,23 @@ class SearchCriteria extends React.Component {
     });
   }
 
-  updateCheckbox(e) {
-    const fieldName = e.target.dataset.field;
+  updateNumberRangeField(m, e) {
+    const field = e.target.dataset.field;
+    const value = e.target.value;
+    let { criteria } = this.state;
+    criteria[field][m] = value;
+    this.setState({
+      criteria
+    });
+  }
+
+  updateCheckbox(field, e) {
     const checked = e.target.checked;
     let { criteria } = this.state;
     if (checked) {
-      criteria[fieldName] = this.initializeObject(fieldName);
+      criteria[field.name] = this.initializeObject(field);
     } else {
-      delete criteria[fieldName];
+      delete criteria[field.name];
     }
     this.setState({
       criteria
@@ -69,14 +92,32 @@ class SearchCriteria extends React.Component {
     });
   }
 
-  initializeObject(fieldName) {
-    const propsField = this.props.fields.filter((field) => field.name === fieldName)[0];
+  initializeObject(field) {
+    const { name, dbName } = field;
     let result = {};
-    result.value = ''; // TODO: different default values for different field types
-    if (propsField.dbName) {
-      result.dbName = propsField.dbName;
+    switch (field.type) {
+      case 'number range':
+        result.minValue = 0;
+        result.maxValue = 1;
+        break;
+      default:
+        result.value = '';
+    }
+    if (dbName) {
+      result.dbName = dbName;
     }
     return result;
+  }
+
+  isFieldActive(field) {
+    const { criteria } = this.state;
+    const keyExists = Object.keys(criteria).indexOf(field.name) > -1;
+    switch (field.type) {
+      case 'number range':
+        return keyExists && Object.keys(criteria[field.name]).indexOf('minValue') > -1;
+      default:
+        return keyExists && Object.keys(criteria[field.name]).indexOf('value') > -1;
+    }
   }
 
   render() {
@@ -97,15 +138,14 @@ class SearchCriteria extends React.Component {
       <div>
         {
           this.props.fields.map((field) => {
-            const keyExists = Object.keys(this.state.criteria).indexOf(field.name) > -1;
-            const disabled = !keyExists;
+            const fieldActive = this.isFieldActive(field);
             return(
-              <div key={ field.name } className={ disabled ? 'disabled' : '' }>
+              <div key={ `${field.name}${field.type === 'number range' ? '-range' : ''}` } className={ fieldActive ? '' : 'disabled' }>
                 <div className="row">
                   <div className="col-xs-1">
-                    <input type="checkbox" onChange={ this.updateCheckbox.bind(this) } data-field={ field.name } checked={ keyExists } />
+                    <input type="checkbox" onChange={ this.updateCheckbox.bind(this, field) } data-field={ field.name } checked={ fieldActive } />
                   </div>
-                  { this.renderField.call(this, field, disabled) }
+                  { this.renderField.call(this, field, fieldActive) }
                 </div>
                 <style jsx>{`
                     .disabled h2 {
@@ -125,25 +165,55 @@ class SearchCriteria extends React.Component {
     );
   }
 
-  renderField(field, disabled) {
+  renderField(field, fieldActive) {
+    const { criteria } = this.state;
     const columnHeader = field.columnHeader || ChangeCase.titleCase(field.name);
-    const value = this.state.criteria[field.name] ? this.state.criteria[field.name].value : '';
+    const value = (criteria[field.name] && criteria[field.name].hasOwnProperty('value')) ? criteria[field.name].value : '';
     switch (field.type) {
       case 'static dropdown':
         return(
           <div className={ `col-xs-${field.columnWidth} `}>
             <h2>{ columnHeader }</h2>
-            <select onChange={ this.updateField.bind(this) } data-field={ field.name } value={ value } disabled={ disabled }>
+            <select onChange={ this.updateField.bind(this) } data-field={ field.name } value={ value } disabled={ !fieldActive }>
               { this.renderOptions(field.options) }
             </select>
             <div className="no-dropdown-field-error" />
           </div>
         );
+      case 'number range':
+        const minValue = (criteria[field.name] && criteria[field.name].hasOwnProperty('minValue')) ? criteria[field.name].minValue : '';
+        const maxValue = (criteria[field.name] && criteria[field.name].hasOwnProperty('maxValue')) ? criteria[field.name].maxValue : '';
+        const minValueIsNumber = this.stringIsNumber(minValue);
+        const maxValueIsNumber = this.stringIsNumber(maxValue);
+        const invalidRange = minValueIsNumber && maxValueIsNumber && +minValue > +maxValue;
+        const minValueRed = fieldActive && (!minValueIsNumber || invalidRange);
+        const maxValueRed = fieldActive && (!maxValueIsNumber || invalidRange);
+        return(
+          <>
+            <div className={ `col-xs-${field.columnWidth} `}>
+              <h2>{ columnHeader }</h2>
+              Min: <input className={ `number-range min ${minValueRed ? 'red' : ''}` } onChange={ this.updateNumberRangeField.bind(this, 'minValue') } data-field={ field.name } value={ minValue } disabled={ !fieldActive } />
+              Max: <input className={ `number-range ${maxValueRed ? 'red' : ''}` } onChange={ this.updateNumberRangeField.bind(this, 'maxValue') } data-field={ field.name } value={ maxValue } disabled={ !fieldActive } />
+              <div className="no-field-error" />
+            </div>
+            <style jsx>{`
+                .number-range {
+                  width: 100px !important;
+                }
+                .min {
+                  margin-right: 15px;
+                }
+                .red {
+                  border: solid 1px red !important;
+                }
+            `}</style>
+          </>
+        );
       default:
         return(
           <div className={ `col-xs-${field.columnWidth} `}>
             <h2>{ columnHeader }</h2>
-            <input onChange={ this.updateField.bind(this) } data-field={ field.name } value={ value } disabled={ disabled } />
+            <input onChange={ this.updateField.bind(this) } data-field={ field.name } value={ value } disabled={ !fieldActive } />
             <div className="no-field-error" />
           </div>
         );
