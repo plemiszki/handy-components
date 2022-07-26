2955790b9b3d8117a9e6d255e9359cecac4e9cd6
Peter Lemiszki <plemiszki@gmail.com>
2022-07-25 21:51:08 -0400

    confirm delete - functional component

--- src/containers/confirm-delete.jsx
+++ src/containers/confirm-delete.jsx
@@ -1,26 +1,23 @@
 import React from 'react'
 import ChangeCase from 'change-case'
 
-class ConfirmDelete extends React.Component {
+export default function ConfirmDelete(props) {
 
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
+  const {
+    closeModal,
+    confirmDelete,
+    entityName,
+  } = props
 
-  render() {
-    return(
-      <div className="confirm-delete">
-        <h1>Are you sure you want to permanently delete this { ChangeCase.noCase(this.props.entityName) }&#63;</h1>
-        <a className="btn red-button" onClick={ this.props.confirmDelete.bind(this) }>
-          Yes
-        </a>
-        <a className="btn" onClick={ this.props.closeModal.bind(this) }>
-          No
-        </a>
-      </div>
-    );
-  }
+  return(
+    <div className="confirm-delete">
+      <h1>Are you sure you want to permanently delete this { ChangeCase.noCase(entityName) }&#63;</h1>
+      <a className="btn red-button" onClick={ confirmDelete }>
+        Yes
+      </a>
+      <a className="btn" onClick={ closeModal }>
+        No
+      </a>
+    </div>
+  )
 }
-
-export default ConfirmDelete;
