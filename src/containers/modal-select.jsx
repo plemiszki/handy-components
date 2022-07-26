49504cda150294bf7243a1139c9f087b796490d5
Peter Lemiszki <plemiszki@gmail.com>
2022-07-25 22:11:09 -0400

    modal select - functional component

--- src/containers/modal-select.jsx
+++ src/containers/modal-select.jsx
@@ -1,43 +1,33 @@
-import React, { Component } from 'react'
-import Common from './modules/common.jsx'
+import React, { useState, useEffect } from 'react'
 import Index from './modules/index.js'
 import HandyTools from 'handy-tools'
 
-class ModalSelect extends React.Component {
-  constructor(props) {
-    super(props);
+export default function ModalSelect(props) {
 
-    this.state = {
-      searchText: ''
-    };
-  }
+  const {
+    func,
+    noneOption,
+    options,
+    property,
+  } = props
 
-  render() {
-    const { func, options, property } = this.props;
-    const { searchText } = this.state;
-    const filteredOptions = Index.filterSearchText({ entities: options, text: searchText, property });
-    return(
-      <div className="modal-select handy-component">
-        <input className="search-box" onChange={ Common.changeStateToTarget.bind(this, 'searchText') } value={ searchText } data-field="searchText" />
-        <ul className="licensor-modal-list">
-          { this.renderNoneOption() }
-          { HandyTools.alphabetizeArrayOfObjects(filteredOptions, property).map((option, index) => {
-            return(
-              <li key={ index } onClick={ () => { func(option) } } data-id={ option.id } data-type={ option.itemType }>{ option[property] }</li>
-            );
-          }) }
-        </ul>
-      </div>
-    );
-  }
+  const [searchText, setSearchText] = useState('')
 
-  renderNoneOption() {
-    if (this.props.noneOption) {
-      return(
-        <li onClick={ this.props.func } data-id={ null } data-type={ null }>(None)</li>
-      );
-    }
-  }
+  const filteredOptions = Index.filterSearchText({ entities: options, text: searchText, property })
+
+  return(
+    <div className="modal-select handy-component">
+      <input className="search-box" onChange={ (e) => { setSearchText(e.target.value) } } value={ searchText } data-field="searchText" />
+      <ul className="licensor-modal-list">
+        { noneOption && (
+          <li onClick={ func } data-id={ null } data-type={ null }>(None)</li>
+        ) }
+        { HandyTools.alphabetizeArrayOfObjects(filteredOptions, property).map((option, index) => {
+          return(
+            <li key={ index } onClick={ () => { func(option) } } data-id={ option.id } data-type={ option.itemType }>{ option[property] }</li>
+          )
+        }) }
+      </ul>
+    </div>
+  )
 }
-
-export default ModalSelect;
