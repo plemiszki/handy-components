c43fbfd575f8431583f5c37228f26b405429aadf
Peter Lemiszki <plemiszki@gmail.com>
2022-09-23 19:05:57 -0400

    sendRequest, fetchEntities

--- src/containers/utils/requests.js
+++ src/containers/utils/requests.js
@@ -2,6 +2,31 @@ import { parseUrl } from './extract'
 import { snakeCase } from 'change-case'
 import { convertObjectKeysToUnderscore } from './convert'
 
+export const sendRequest = (url, args = {}) => {
+    const { method = 'GET', data } = args;
+    let fetchArgs = { method };
+    if (method === 'GET' && data) {
+        url = `${url}?${new URLSearchParams(convertObjectKeysToUnderscore(data))}`;
+    }
+    if (method !== 'GET') {
+        const headers = {
+            'x-csrf-token': getCsrfToken(),
+            'Content-Type': 'application/json',
+        }
+        fetchArgs = { ...fetchArgs, headers }
+        if (data) {
+            fetchArgs = { ...fetchArgs, body: JSON.stringify(convertObjectKeysToUnderscore(data)) }
+        }
+    }
+    return fetch(url, fetchArgs).then(async (response) => {
+        const payload = await response.json();
+        if (!response.ok) {
+            return Promise.reject(payload);
+        }
+        return Promise.resolve(payload);
+    })
+}
+
 export const fetchEntity = (args = {}) => {
     const {
         directory = parseUrl()[1],
@@ -10,9 +35,21 @@ export const fetchEntity = (args = {}) => {
     return fetch(`/api/${directory}/${id}`).then(data => data.json())
 }
 
+export const fetchEntities = (args = {}) => {
+    const {
+        directory = parseUrl()[0],
+        data,
+    } = args;
+    let url = `/api/${directory}`;
+    if (data) {
+        url = `${url}?${new URLSearchParams(convertObjectKeysToUnderscore(data))}`
+    }
+    return fetch(url).then(data => data.json())
+}
+
 export const createEntity = (args = {}) => {
     const {
-        directory,
+        directory = parseUrl()[0],
         entityName,
         entity,
     } = args;
--- src/index.js
+++ src/index.js
@@ -27,7 +27,7 @@ import { pluckFromObjectsArray, parseUrl } from './containers/utils/extract'
 import { todayDMY } from './containers/utils/misc'
 import { removeFromArray } from './containers/utils/mutate'
 import { resetNiceSelect, setUpNiceSelect } from './containers/utils/nice-select'
-import { fetchEntity, createEntity, updateEntity, deleteEntity, getCsrfToken } from './containers/utils/requests.js'
+import { fetchEntity, fetchEntities, createEntity, updateEntity, deleteEntity, getCsrfToken, sendRequest } from './containers/utils/requests.js'
 import { alphabetizeArrayOfObjects, commonSort, sortArrayOfObjects } from './containers/utils/sort'
 
 const ModalSelectStyles = {
@@ -58,6 +58,7 @@ export {
   Details,
   ellipsis,
   fetchEntity,
+  fetchEntities,
   getCsrfToken,
   FullIndex,
   Index,
@@ -76,6 +77,7 @@ export {
   resetNiceSelect,
   SearchCriteria,
   SearchIndex,
+  sendRequest,
   setUpNiceSelect,
   SimpleDetails,
   sortArrayOfObjects,
