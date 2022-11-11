62b782e34685c2e85b86eb307035b8bd92b1f2e6
Peter Lemiszki <plemiszki@gmail.com>
2022-11-11 15:06:38 -0500

    move confirmation modal into delete button

--- src/containers/delete-button.jsx
+++ src/containers/delete-button.jsx
@@ -1,12 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Common from './modules/common.jsx'
+import Modal from 'react-modal'
+import ConfirmDelete from './confirm-delete'
+
+const deleteModalStyles = {
+	overlay: {
+		background: 'rgba(0, 0, 0, 0.50)'
+	},
+	content: {
+		background: '#FFFFFF',
+		margin: 'auto',
+		maxWidth: 540,
+		height: 207,
+		border: 'solid 1px red',
+		borderRadius: '6px',
+		textAlign: 'center',
+		color: '#5F5F5F',
+		paddingTop: '36px'
+	}
+};
 
 export default function DeleteButton(props) {
-	const { styles, onClick, disabled = false } = props;
+	const { styles, confirmDelete, disabled = false, entityName } = props;
+	const [modalOpen, setModalOpen] = useState(false);
 
 	return(
 		<>
-			<a style={ styles } className={ `${Common.renderDisabledButtonClass(disabled)}` } onClick={ () => onClick() }>Delete</a>
+			<a style={ styles } className={ `${Common.renderDisabledButtonClass(disabled)}` } onClick={ () => setModalOpen(true) }>Delete</a>
+			<Modal isOpen={ modalOpen } onRequestClose={ () => { setModalOpen(false) } } contentLabel="Modal" style={ deleteModalStyles }>
+				<ConfirmDelete
+					entityName={ entityName }
+					confirmDelete={ (e) => { confirmDelete(e) } }
+					closeModal={ () => { setModalOpen(false) } }
+				/>
+			</Modal>
 			<style jsx>{`
 				a {
 						float: right;
