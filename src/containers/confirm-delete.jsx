74a9de4f98d33dae5b1fdcc9f54bbf91ee9e8581
Peter Lemiszki <plemiszki@gmail.com>
2023-02-19 14:11:19 -0500

    confirm delete modal

--- src/containers/confirm-delete.jsx
+++ src/containers/confirm-delete.jsx
@@ -1,6 +1,24 @@
 import React from 'react'
 import ChangeCase from 'change-case'
 import Button from './button.jsx'
+import Modal from 'react-modal'
+
+const deleteModalStyles = {
+	overlay: {
+		background: 'rgba(0, 0, 0, 0.50)'
+	},
+	content: {
+		background: '#FFFFFF',
+		margin: 'auto',
+		maxWidth: 540,
+		height: 207,
+		border: 'solid 1px red',
+		borderRadius: '6px',
+		textAlign: 'center',
+		color: '#5F5F5F',
+		paddingTop: '36px'
+	}
+};
 
 export default function ConfirmDelete(props) {
 
@@ -8,28 +26,31 @@ export default function ConfirmDelete(props) {
     closeModal,
     confirmDelete,
     entityName,
+    isOpen,
   } = props
 
   return (
     <>
-      <div className="confirm-delete">
-        <h1>Are you sure you want to permanently delete this { ChangeCase.noCase(entityName) }&#63;</h1>
-        <Button
-          text="Yes"
-          onClick={ confirmDelete }
-          style={
-            {
-              backgroundColor: '#B40404',
-              marginRight: 40,
+      <Modal isOpen={ isOpen } onRequestClose={ closeModal } contentLabel="Modal" style={ deleteModalStyles }>
+        <div className="confirm-delete">
+          <h1>Are you sure you want to permanently delete this { ChangeCase.noCase(entityName) }&#63;</h1>
+          <Button
+            text="Yes"
+            onClick={ confirmDelete }
+            style={
+              {
+                backgroundColor: '#B40404',
+                marginRight: 40,
+              }
             }
-          }
-          hoverColor="red"
-        />
-        <Button
-          text="No"
-          onClick={ closeModal }
-        />
-      </div>
+            hoverColor="red"
+          />
+          <Button
+            text="No"
+            onClick={ closeModal }
+          />
+        </div>
+      </Modal>
       <style jsx>{`
         h1 {
           color: #2C2F33;
--- src/containers/delete-button.jsx
+++ src/containers/delete-button.jsx
@@ -1,24 +1,6 @@
 import React, { useState } from 'react'
 import Common from './modules/common.jsx'
-import Modal from 'react-modal'
 import ConfirmDelete from './confirm-delete'
-
-const deleteModalStyles = {
-	overlay: {
-		background: 'rgba(0, 0, 0, 0.50)'
-	},
-	content: {
-		background: '#FFFFFF',
-		margin: 'auto',
-		maxWidth: 540,
-		height: 207,
-		border: 'solid 1px red',
-		borderRadius: '6px',
-		textAlign: 'center',
-		color: '#5F5F5F',
-		paddingTop: '36px'
-	}
-};
 
 export default function DeleteButton(props) {
 	const { styles, confirmDelete, disabled = false, entityName } = props;
@@ -27,16 +9,15 @@ export default function DeleteButton(props) {
 	return(
 		<>
 			<a style={ styles } className={ `${Common.renderDisabledButtonClass(disabled)}` } onClick={ () => setModalOpen(true) }>Delete</a>
-			<Modal isOpen={ modalOpen } onRequestClose={ () => { setModalOpen(false) } } contentLabel="Modal" style={ deleteModalStyles }>
-				<ConfirmDelete
-					entityName={ entityName }
-					confirmDelete={ (e) => {
-						setModalOpen(false)
-						confirmDelete(e)
-					} }
-					closeModal={ () => { setModalOpen(false) } }
-				/>
-			</Modal>
+			<ConfirmDelete
+				isOpen={ modalOpen }
+				entityName={ entityName }
+				confirmDelete={ (e) => {
+					setModalOpen(false)
+					confirmDelete(e)
+				} }
+				closeModal={ () => { setModalOpen(false) } }
+			/>
 			<style jsx>{`
 				a {
 						float: right;
