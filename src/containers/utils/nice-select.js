bbf2b2d51f0a84f54286e5fdba997a731ef0a0f2
Peter Lemiszki <plemiszki@gmail.com>
2024-09-22 12:18:59 -0400

    pretty

--- src/containers/utils/nice-select.js
+++ src/containers/utils/nice-select.js
@@ -1,15 +1,19 @@
 export const resetNiceSelect = (obj) => {
-    // assumes jQuery (and nice select) have been loaded via script tags
-    var $dropDowns = $(obj.selector);
-    $dropDowns.niceSelect('destroy');
-    $dropDowns.unbind('change');
-    $dropDowns.niceSelect().on('change', obj.func);
-}
+  // assumes jQuery (and nice select) have been loaded via script tags
+  var $dropDowns = $(obj.selector);
+  $dropDowns.niceSelect("destroy");
+  $dropDowns.unbind("change");
+  $dropDowns.niceSelect().on("change", obj.func);
+};
 
 export const setUpNiceSelect = (obj) => {
-    // assumes jQuery (and nice select) have been loaded via script tags
-    var $dropDowns = $(obj.selector);
-    if ($dropDowns[0] && (!$dropDowns[0].nextSibling || !$dropDowns[0].nextSibling.classList.contains('nice-select'))) {
-      $dropDowns.niceSelect().on('change', obj.func);
-    }
-}
+  // assumes jQuery (and nice select) have been loaded via script tags
+  var $dropDowns = $(obj.selector);
+  if (
+    $dropDowns[0] &&
+    (!$dropDowns[0].nextSibling ||
+      !$dropDowns[0].nextSibling.classList.contains("nice-select"))
+  ) {
+    $dropDowns.niceSelect().on("change", obj.func);
+  }
+};
